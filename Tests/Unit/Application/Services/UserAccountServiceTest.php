f0337ad431fc87b095385c7082b352cb5e6814f2
Ruben Zubasti <rubenix2.0@gmail.com>
2025-04-14 17:57:48 +0200

    [] - Primera iteracion

---
+++ Tests/Unit/Application/Services/UserAccountServiceTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TwitchAnalytics\Application\Services;
+
+use PHPUnit\Framework\TestCase;
+
+class UserAccountServiceTest extends TestCase
+{
+
+    /**
+     * @test
+     **/
+    public function testGetAccountAge()
+    {
+        $userAccountService = new
+    }
+}
