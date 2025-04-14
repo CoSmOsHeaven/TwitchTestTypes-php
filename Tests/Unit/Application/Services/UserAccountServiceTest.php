468fbc80d6784b0a2a0e786a4aa819577292416d
CoSmOsHeaven <rubenix2.0@gmail.com>
2025-04-14 19:01:58 +0200

    [] - Tests unitarios de GetAccountAge implementados

--- Tests/Unit/Application/Services/UserAccountServiceTest.php
+++ Tests/Unit/Application/Services/UserAccountServiceTest.php
@@ -4,9 +4,7 @@ namespace TwitchAnalytics\Application\Services;
 
 use Mockery;
 use PHPUnit\Framework\TestCase;
-use TwitchAnalytics\Controllers\GetUserPlatformAge\UserNameValidator;
 use TwitchAnalytics\Domain\Exceptions\UserNotFoundException;
-use TwitchAnalytics\Domain\Interfaces\UserRepositoryInterface;
 use TwitchAnalytics\Domain\Models\User;
 use TwitchAnalytics\Infrastructure\Repositories\ApiUserRepository;
 
@@ -34,25 +32,19 @@ class UserAccountServiceTest extends TestCase
     {
         $createdAt = '2011-11-20T00:00:00Z';
 
-        // Mock del objeto User
-        $userMock = Mockery::mock();
+        $userMock = Mockery::mock(User::class);
         $userMock->shouldReceive('getCreatedAt')->andReturn($createdAt);
         $userMock->shouldReceive('getDisplayName')->andReturn('Ninja');
 
-        // Mock del repositorio
         $userRepositoryMock = Mockery::mock(ApiUserRepository::class);
         $userRepositoryMock->shouldReceive('findByDisplayName')->with('Ninja')->andReturn($userMock);
 
-        // Instanciar el servicio
         $userAccountService = new UserAccountService($userRepositoryMock);
 
-        // Ejecutar
         $result = $userAccountService->getAccountAge('Ninja');
 
-        // Calcular valor esperado
         $expectedDays = (new \DateTime($createdAt))->diff(new \DateTime())->days;
 
-        // Assertions
         $this->assertEquals('Ninja', $result['name']);
         $this->assertEquals($expectedDays, $result['days_since_creation']);
         $this->assertEquals($createdAt, $result['created_at']);
